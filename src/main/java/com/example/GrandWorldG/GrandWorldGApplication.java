5f34f602c3a90526e21849ce245db60d62838bc3
zhaoheting <492995407@qq.com>
2020-02-14 16:07:18 +0800

    Init a spring boot project with gradle.

---
+++ src/main/java/com/example/GrandWorldG/GrandWorldGApplication.java
@@ -0,0 +1,13 @@
+package com.example.GrandWorldG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandWorldGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandWorldGApplication.class, args);
+	}
+
+}

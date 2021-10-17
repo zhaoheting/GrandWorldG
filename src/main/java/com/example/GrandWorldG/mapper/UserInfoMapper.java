f09e494960b16f1edd80538048024da6e480f899
zhaoheting <492995407@qq.com>
2021-10-17 20:01:46 +0800

    Define mapper.

---
+++ src/main/java/com/example/GrandWorldG/mapper/UserInfoMapper.java
@@ -0,0 +1,23 @@
+package com.example.GrandWorldG.mapper;
+
+import com.example.GrandWorldG.entity.UserInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * User info mapper.
+ *
+ * @author HeTing.Zhao
+ * @since 2021/10/17
+ **/
+@Mapper
+public interface UserInfoMapper {
+
+    /**
+     * @param userId
+     * @return com.example.GrandWorldG.entity.UserInfo
+     * @author HeTing.Zhao
+     * @since 2021/10/17
+     **/
+    UserInfo selectUserInfoByUserId(int userId);
+}

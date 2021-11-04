13dc9db2296d2fdd9329aba68b4438a33409920a
zhaoheting <492995407@qq.com>
2021-11-04 21:52:08 +0800

    code clean.

--- src/main/java/com/example/GrandWorldG/controller/UserInfoController.java
+++ src/main/java/com/example/GrandWorldG/controller/UserInfoController.java
@@ -5,23 +5,17 @@ import com.example.GrandWorldG.model.PageableModel;
 import com.example.GrandWorldG.service.UserInfoService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * {@link UserInfo} controller.
  * Cross origin annotation is used to solve cross domain issues. It is effective than setting headers for response.
  * But we need to add this annotation for every controller.
- * There is a better approach that override "addCorsMappings‘ method of interface "WebMvcConfigurer".
+ * There is a better approach that override "addCorsMappings" method of interface "WebMvcConfigurer".
  *
  * @author HeTing.Zhao
  * @since 2021/10/17
@@ -41,7 +35,7 @@ public class UserInfoController {
     public ResponseEntity<PageInfo<UserInfo>> getAllUserInfoInPage(PageableModel<UserInfo> pageableModel) {
         PageHelper.startPage(pageableModel.getPageNum(), pageableModel.getPageSize());
         List<UserInfo> userInfoList = userInfoService.getAllUserInfo();
-        PageInfo<UserInfo> pageInfo = new PageInfo(userInfoList);
+        PageInfo<UserInfo> pageInfo = new PageInfo<>(userInfoList);
         return ResponseEntity.ok().body(pageInfo);
     }
 
